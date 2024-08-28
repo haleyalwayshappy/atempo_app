d0142de7154a54e7726246406e78921a5b98f4d7
haley <78947754+haleyalwayshappy@users.noreply.github.com>
2024-08-28 19:38:28 +0900

    리스트 뷰 만들기

--- lib/view/pages/home/home_screen.dart
+++ lib/view/pages/home/home_screen.dart
@@ -19,8 +19,9 @@ class HomeScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              RecommendMusic(),
               Container(
+                margin: EdgeInsets.all(8.0),
+                padding: EdgeInsets.all(8.0),
                 width: double.infinity,
                 height: 80,
                 color: Colors.yellowAccent,
@@ -31,6 +32,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              RecommendMusic(),
               // 음악 가로 위젯
             ],
           ),
@@ -50,7 +52,7 @@ class RecommendMusic extends StatelessWidget {
       {
         'index': '1',
         'image': 'assets/images/maru_image.png',
-        'text': 'Music 1'
+        'text': 'Musssssic 1'
       },
       {
         'index': '2',
@@ -75,33 +77,39 @@ class RecommendMusic extends StatelessWidget {
     ];
 
     return Container(
-      color: Colors.purple,
+      // color: Colors.purple,
       width: double.infinity,
       height: 500,
-      child: ListView(
-        // physics: BouncingScrollPhysics(),
-        // scrollDirection: Axis.horizontal,
-        children: [
-          ListView.builder(
-            itemCount: musicItems.length,
-            itemBuilder: (context, idx) {
-              var imageRoute = musicItems[idx]['image'];
-              var musicTexts = musicItems[idx]['text'];
-              print("musicText $musicTexts");
-              return SizedBox(height: 50, width: 50, child: Text("asdf")
-                  // Column(
-                  //   children: [
-                  //     // Image.asset(imageRoute!),
-                  //     // Text(
-                  //     //   musicTexts!,
-                  //     //   style: TextStyle(color: Colors.purple),
-                  //     // ),
-                  //   ],
-                  // ),
-                  );
-            },
-          ),
-        ],
+      child: ListView.builder(
+        physics: ClampingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: musicItems.length,
+        itemBuilder: (context, idx) {
+          var imageRoute = musicItems[idx]['image'];
+          var musicTexts = musicItems[idx]['text'];
+          return SizedBox(
+            height: 100,
+            width: 100,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Image.asset(
+                      imageRoute!,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Text(
+                    musicTexts!,
+                    style: TextStyle(color: wFontColor),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
