a52f181e1bef68e035d2a5a06ac435725f61b414
haley <78947754+haleyalwayshappy@users.noreply.github.com>
2024-09-25 02:42:29 +0900

    바보같이 풀안하고 파이어베이스를 설정해버렸다!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
+<<<<<<< Updated upstream
 import 'package:atempo_app/view/pages/home/choice_emotion_screen.dart';
+=======
+import 'package:atempo_app/firebase_options.dart';
+import 'package:atempo_app/screens/account/login_email_screen.dart';
+import 'package:atempo_app/screens/account/login_screen.dart';
+import 'package:atempo_app/screens/diary/diary_list_screen.dart';
+import 'package:atempo_app/screens/diary/diary_main_screen.dart';
+import 'package:atempo_app/screens/home/choice_emotion_screen.dart';
+import 'package:atempo_app/screens/home/home_screen.dart';
+import 'package:atempo_app/utils/constants.dart';
+import 'package:atempo_app/widgets/bottom_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+>>>>>>> Stashed changes
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MainScreenApp());
+void main()async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainScreenApp());
+}
 
 class MainScreenApp extends StatelessWidget {
   const MainScreenApp({super.key});
