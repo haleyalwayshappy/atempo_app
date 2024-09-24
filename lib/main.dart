1b7f98831e5e0a014d6116d778807a2737c00744
haley <78947754+haleyalwayshappy@users.noreply.github.com>
2024-09-25 03:47:52 +0900

    파이어베이스 연동
    근데 이제 오류를 곁들인..

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:atempo_app/firebase_options.dart';
 import 'package:atempo_app/utils/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:atempo_app/model/router.dart';
 
-void main() => runApp(MainScreenApp());
+
+void main(){
+
+  runApp(MainScreenApp());
+}
 
 class MainScreenApp extends StatelessWidget {
   MainScreenApp({Key? key}) : super(key: key);
@@ -31,3 +37,42 @@ class MainScreenApp extends StatelessWidget {
     );
   }
 }
+
+/*
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MainScreenApp());
+}
+
+class MainScreenApp extends StatelessWidget {
+  MainScreenApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: router,
+      theme: ThemeData(
+        // appBarTheme: AppBarTheme(
+        //   actionsIconTheme: IconThemeData(
+        //     color: mEnabledColor,
+        //     size: 32,
+        //   ),
+        //   backgroundColor: mBackgroundColor,
+        //   centerTitle: false,
+        //   titleTextStyle: TextStyle(
+        //     color: mFontDarkColor,
+        //     fontSize: 24,
+        //     fontWeight: FontWeight.w800,
+        //     letterSpacing: 4.0,
+        //   ),
+        // ),
+        scaffoldBackgroundColor: mBackgroundColor,
+      ),
+    );
+  }
+}
+*/
